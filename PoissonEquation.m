4c3baff0ffac21a4873da5711e28de938f770c51
German <groblesbailon@gmail.com>
2018-04-30 22:16:42 -0500

    added a few parameters and variables

--- PoissonEquation.m
+++ PoissonEquation.m
@@ -3,7 +3,21 @@
 %Poisson Equation Project
 %Scientific Computing for Mechanical Engineers
 
-clear all
+clearvars
 clc
 close all
 %% Variables
+
+ax = -pi;
+ay = -pi;
+bx = pi;
+by = pi;
+Lx = bx - ax;
+Ly = by - ay;
+
+Nx = 50;    %nodes in the x direction
+Ny = 50;    %nodes in the y direction
+dx = (Lx)/(Nx+1);
+dy = (Ly)/(Ny+1);
+
+
