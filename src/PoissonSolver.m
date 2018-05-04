4687fb506596910a91bf23fabf0f4fa29a9b6c1d
German <groblesbailon@gmail.com>
2018-05-04 01:35:24 -0500

    boundary conditions for the problem

--- src/PoissonSolver.m
+++ src/PoissonSolver.m
@@ -22,3 +22,15 @@ Ny = 200;                %nodes in the y direction
 
 hx = linspace(ax,bx,Nx);        %equally spaced vector in x direction
 hy = linspace(ay,by,Ny);        %equally spaced vector in y direction
+
+[x,y] = meshgrid(hx,hy);        %filled matrix from -pi to pi in both direction to simulate rectangle
+y = flipud(y);                  %flipped y vector to have it go from -pi to pi in vertical direction
+
+fa = ((x-ax).^2).*cos(pi.*x/ax);
+ga = x.*((x-ax).^2);
+F = cos((pi/2).*(2.*((x-ax)/(bx-ax))+1)).*sin(pi.*((y-ay)/(by-ay)));                    %right hand side
+uby = fa;                                                                               %top BC
+uay = ga;                                                                               %bottom BC
+ubx = (bx.*((bx-ax).^2))+(((y-ay)/(by-ay)).*((((bx-ax).^2).*cos(pi.*bx/ax))-(bx.*((bx-ax).^2))));   %Right BC
+
+
