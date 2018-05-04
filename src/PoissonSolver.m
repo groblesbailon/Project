938bcc2b5d69922cb6b2d397c7788963023f7aaf
German <groblesbailon@gmail.com>
2018-05-04 01:34:19 -0500

    variables for the problem

--- src/PoissonSolver.m
+++ src/PoissonSolver.m
@@ -10,3 +10,15 @@ close all
 
 %% variables 
 
+ax = -pi;
+ay = -pi;
+bx = pi;
+by = pi;
+Lx = bx - ax;
+Ly = by - ay;
+
+Nx = 200;                %nodes in the x direction
+Ny = 200;                %nodes in the y direction
+
+hx = linspace(ax,bx,Nx);        %equally spaced vector in x direction
+hy = linspace(ay,by,Ny);        %equally spaced vector in y direction
